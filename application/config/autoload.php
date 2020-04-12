6f406dfe261f86c31ae14623fb8a41ed1836ed1d
rtgi-developers <58165254+rtgi-developers@users.noreply.github.com>
2020-04-12 19:48:08 +0530

    Several improvements in system errors view and updated autoload with custom configuration.

--- application/config/autoload.php
+++ application/config/autoload.php
@@ -103,7 +103,7 @@ $autoload['helper'] = array('url', 'string');
 | config files.  Otherwise, leave it blank.
 |
 */
-$autoload['config'] = array();
+$autoload['config'] = array('config_custom');
 
 /*
 | -------------------------------------------------------------------
--- application/config/config_custom.php
+++ application/config/config_custom.php
@@ -3,11 +3,11 @@
 defined('BASEPATH') OR exit("No direct script access allowed.");
 
 // Website details
-$config['title']   = "";
-$config['company'] = "";
+$config['title']   = "Codeigniter Project";
+$config['company'] = "Muhammad Tarique";
 
 // System emails
-$config['email_careers'] = "careers@rituraj.com";
-$config['email_contact'] = "contact@rituraj.com";
+$config['email_contact'] = "contact@ciproject.com";
+$config['email_careers'] = "careers@ciproject.com";
 
 ?>
